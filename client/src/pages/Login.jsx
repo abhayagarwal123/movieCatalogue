54adbcc69da6c7d1608d744de976f1c95f9cf035
Nabil Ibarissen <nabilibarissen@Sansfil-Eduroam-Etudiants-57-2.polymtl.ca>
2023-08-28 19:42:49 -0400

    new features, need to finish star css for rating

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Form from '../components/Form'
+import {useCookies} from 'react-cookie'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 export default function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [_, setCookies] = useCookies(["access_token"])
+
+  const navigate = useNavigate()
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', {
+        username,
+        password
+      })
+
+      if (!response.data.token) {
+        alert(response.data.message)
+        setUsername('')
+        setPassword('')
+      } else {
+        setCookies("access_token", response.data.token)
+        window.localStorage.setItem('userID', response.data.userID)
+        window.localStorage.setItem('username', username)
+        navigate('/')
+      }
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+
+
   return (
-    <div>Login</div>
+
+    <div>
+      <Form username={username}
+      setUsername = {setUsername}
+      password={password}
+      setPassword={setPassword}
+      label={"Login"}
+      onSubmit={onSubmit}
+      />
+    </div>
   )
 }
