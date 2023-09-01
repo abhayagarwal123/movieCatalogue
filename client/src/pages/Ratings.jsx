d6a7480b76fe347d859ea54e68a6cfe7bd274bd3
Nabil Ibarissen <nabilibarissen@Sansfil-Eduroam-Etudiants-57-2.polymtl.ca>
2023-09-01 13:57:21 -0400

    Worked on backend side of the project, added comments on backend and added the rating movie feature, you can now rate a movie and add it to a user in the database.

--- client/src/pages/Ratings.jsx
+++ client/src/pages/Ratings.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export default function Ratings() {
+  const [cookies] = useCookies(['access_token']);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!cookies.access_token) {
+      navigate('/login')
+    }
+  }, [cookies.access_token, history]);
+
+  if (!cookies.access_token) {
+    return null;
+  }
+
   return (
     <div>Ratings</div>
-  )
+  );
 }
